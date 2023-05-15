d49861ddb405fb6406d5bf5d533f836a270b1775
hyunseo <hsjeong17859@acornsoft.io>
2023-05-15 13:21:15 +0900

    first commit

--- src/main/java/com/api/userModify.java
+++ src/main/java/com/api/userModify.java
@@ -10,32 +10,37 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class userModify extends Thread {
-    public static void user_Modify(JSONObject userId, JSONObject data, String accountSeq) throws ParseException {
+    public static void user_Modify(JSONObject userId, JSONObject Datas, String accountSeq) throws ParseException {
         // class
         Thread userInactive = new userInactive();
         Thread getADUsers = new getADUsers();
         JSONObject allUsers = ((getADUsers) getADUsers).get_ADUsers();
         JSONParser parser = new JSONParser();
 
+        //JSONObject Update
         JSONObject jsonObj1 = (JSONObject) parser.parse(userId.toString());
         JSONObject jsonObj2 = (JSONObject) parser.parse(allUsers.toString());
         JSONArray jsonarry1 = (JSONArray) jsonObj1.get("result");
 
+        //update
         JSONObject update = new JSONObject();
         JSONArray roles = new JSONArray();
         int count = 0;
-        roles.add(0, "DEVOPS");
+        roles.add(0, "DEV"); // DEV 추가
 
         for (Object obj : jsonarry1) {
+            //API 사용자 정보 정보
             JSONObject apiUser = (JSONObject) obj;
             String apiUserId = apiUser.get("userId").toString();
-            String apiUserDepartment = apiUser.containsKey("userDepartment") ? apiUser.get("userDepartment").toString() : "null";
+            String apiUserDepartment = apiUser.containsKey("userDepartment") ? apiUser.get("userDepartment").toString() : "null"; 
 
+            //AD 사용자 정보
             for (Object adUserObj : allUsers.values()) {
                 JSONObject adUser = (JSONObject) adUserObj;
                 String adUserDepartment = adUser.get("userDepartment").toString();
                 String adUserId = adUser.get("userId").toString();
 
+                //업데이트할 사용자 정보, AD정보
                 if (apiUserId.equals(adUserId) && !apiUserDepartment.equals(adUserDepartment)) {
                     System.out.println(adUser);
                     update.put(count, adUser);
@@ -43,30 +48,37 @@ public class userModify extends Thread {
                     count++;
 
                     try {
-                        URL url = new URL(System.getenv("API_URL") + "/api/account/" + accountSeq + "/user/" + apiUser.get("userSeq"));
+                        //url
+                        URL url = new URL(System.getenv("COCKTAIL_API") + "/api/account/" + accountSeq + "/user/" + apiUser.get("userSeq"));
+                        //conn
                         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                        //method 방식 : PUT
                         connection.setRequestMethod("PUT");
+
+                        //header setting
                         connection.setRequestProperty("user-id", "1");
                         connection.setRequestProperty("user-role", "ADMIN");
                         connection.setRequestProperty("Content-Type", "application/json;");
                         connection.setDoOutput(true);
 
+                        // 정보 전송
                         try (OutputStream outputStream = connection.getOutputStream();
                              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"))) {
                             writer.write(adUser.toString());
                             writer.flush();
                         }
 
-                        StringBuilder response = new StringBuilder();
+                        StringBuilder sb = new StringBuilder();
                         try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"))) {
                             String line;
                             while ((line = reader.readLine()) != null) {
-                                response.append(line).append("\n");
+                                sb.append(line).append("\n");
                             }
                         }
 
-                        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                            System.out.println("결과: " + response.toString());
+                        if (connection.getResponseCode() == 200) {
+                            System.out.println("결과: " + sb.toString());
                         } else {
                             System.out.println("\"code\": \"" + connection.getResponseCode() + "\"");
                             System.out.println("\"message\": \"" + connection.getResponseMessage() + "\"");
@@ -77,7 +89,7 @@ public class userModify extends Thread {
                 }
             }
         }
-
+        //비활성
         ((userInactive) userInactive).user_Inactive(userId, update, accountSeq);
     }
 }
